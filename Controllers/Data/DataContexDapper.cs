b330e95f656e830574ab8b4932d67debfa29d321
DESKTOP-9QLCHAS\sance <sanceyff@gmail.com>
2026-03-30 06:08:24 +0700

    add all file

---
+++ Controllers/Data/DataContexDapper.cs
@@ -0,0 +1,26 @@
+//Created by my self
+using Dapper;
+using System.Data;
+using Microsoft.Data.SqlClient;
+namespace DotnetAPI 
+{
+    class DataContextDapper
+    {
+        private readonly IConfiguration _config;
+        public DataContextDapper(IConfiguration config)
+        {
+           _config = config;      
+        }
+
+        public IEnumerable<T> LoadData<T>(string sql)
+        {
+            IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            return dbConnection.Query<T>(sql);
+        }
+        public T LoadDataSingle<T>(string sql)
+        {
+            IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            return dbConnection.QuerySingle<T>(sql);
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class UserController : ControllerBase
+{
+    DataContextDapper _dapper;
+
+    public UserController(IConfiguration config)
+    {
+        _dapper = new DataContextDapper(config);
+        //Console.WriteLine(config.GetConnectionString("DefaultConnection"));
+    }
+    [HttpGet("TestConnection")]
+    public DateTime TestConnection()
+    {
+        return _dapper.LoadDataSingle<DateTime>("SELECT DATE()");
+    }
+    [HttpGet("GetUsers/{testValue}")]
+    public string[] GetUsers(string testValue)
+    {
+        string[] responseArray = new string[]{
+            "test1",
+            "test2",
+            "thsi is test"
+        }; 
+        return responseArray;
+    }
+}
